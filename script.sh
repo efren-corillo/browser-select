f9c18ae453abbf9c0e96abbb45eaae1fc212e6c4
efren-corillo <efren.corillo@doublybear.dev>
2023-12-13 14:10:17 +0800

    made the script check which browsers are installed, and only list those browsers.

--- script.sh
+++ script.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-# Define your browsers
-BROWSERS=("Firefox" "Google Chrome" "Chromium" "Opera")
+# Define possible browsers
+declare -A BROWSERS
+BROWSERS["Firefox"]="firefox"
+BROWSERS["Google Chrome"]="google-chrome"
+BROWSERS["Chromium"]="chromium-browser"
+BROWSERS["Opera"]="opera"
+
+# Check for installed browsers
+INSTALLED_BROWSERS=()
+for name in "${!BROWSERS[@]}"; do
+    command=${BROWSERS[$name]}
+    if command -v $command &> /dev/null; then
+        INSTALLED_BROWSERS+=("$name")
+    fi
+done
+
+# Exit if no browsers are found
+if [ ${#INSTALLED_BROWSERS[@]} -eq 0 ]; then
+    zenity --error --text="No web browsers found."
+    exit 1
+fi
 
 # Create the selection menu
-BROWSER=$(zenity --list --title="Select Browser" --column="Browser" "${BROWSERS[@]}")
+BROWSER=$(zenity --list --title="Select Browser" --column="Browser" "${INSTALLED_BROWSERS[@]}")
 
 # Open the URL in the selected browser
-case $BROWSER in
-    "Firefox")
-        firefox "$1"
-        ;;
-    "Google Chrome")
-        google-chrome "$1"
-        ;;
-    "Chromium")
-        chromium "$1"
-        ;;
-    "Opera")
-        opera "$1"
-        ;;
-    *)
-        echo "No browser selected or unrecognized option."
-        exit 1
-        ;;
-esac
+BROWSER_COMMAND=${BROWSERS[$BROWSER]}
+if [ -n "$BROWSER_COMMAND" ]; then
+    $BROWSER_COMMAND "$1"
+else
+    zenity --error --text="No browser selected or unrecognized option."
+    exit 1
+fi
